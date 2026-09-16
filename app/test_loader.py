38d89916d8b8567485d9e72c2fb707683ce6e2da
Sprateek889 <sprateek889@gmail.com>
2026-09-16 13:43:55 +0530

    Created Text splitter: text_splitter.py

--- app/test_loader.py
+++ app/test_loader.py
@@ -1,12 +1,21 @@
 from app.document_loader import load_pdf
+from app.text_splitter import split_documents
 
 documents = load_pdf("data/langchain-guide.pdf")
 
 print("Number of documents:", len(documents))
 
-for i, document in enumerate(documents[:3]):
+chunks = split_documents(documents)
 
-    print("\n" + "=" * 50)
-    print(f"PAGE {i + 1}")
-    print("=" * 50)
-    print(document.page_content[:100])
+print("Chunks:", len(chunks))
+
+for i, chunk in enumerate(chunks[:5]):
+
+    print("\n" + "=" * 60)
+    print(f"CHUNK {i + 1}")
+    print("=" * 60)
+
+    print(chunk.page_content)
+
+    print("\nMetadata:")
+    print(chunk.metadata)
